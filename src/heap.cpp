feea8f179dc407be735158cac37be6db30139505
ssamoyeds <165983028+ssamoyeds@users.noreply.github.com>
2024-04-22 11:59:07 -0400

    Create heap.cpp

---
+++ src/heap.cpp
@@ -0,0 +1,68 @@
+//
+// Created by Gregory Nobleza on 4/17/2024.
+//
+
+#include "heap.h"
+
+void Heap::heapify(int node) {
+    int parent = (node - 1)/2;
+
+    if(node > size){
+        //not in the heap
+        return;
+    }
+    if(node == 1){
+        //top node
+        return;
+    }
+    if((container[node] > container[parent]) == 1){
+        std::swap(container[node], container[parent]);
+
+        heapify(parent);
+    }
+}
+
+Heap::Triplet::Triplet(float _score, const std::string _domain, const std::string _ip){
+    score = _score;
+    domain = _domain;
+    ip = _ip;
+}
+void Heap::insert(float score, const std::string& domain, const std::string& ip){
+    struct Triplet newTriplet(score, domain, ip);
+
+    container.push_back(newTriplet);
+    size++;
+
+    heapify(size-1);
+}
+
+Heap::Triplet Heap::popTop(){
+    Triplet saveTop = container[0];
+    std::swap(container[0], container[size-1]);
+    size--;
+    heapifyDown(0);
+
+
+}
+
+void Heap::heapifyDown(int node) {
+    if(node > size){
+        return;
+    }
+
+    int swap = node;
+
+    if(node*2+1 <= size && (container[node*2+1] > container[node]) == 1){
+        swap = node*2+1;
+    }
+    if((node*2)+2 <= size && (container[node*2+2] > container[node]) == 1){
+        swap = node*2 + 2;
+    }
+
+    if(swap != node){
+        std::swap(container[node], container[swap]);
+        heapifyDown(swap);
+    }
+
+
+}
